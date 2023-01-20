b225d7fd4d251a390f5d4b32e3271d07505d386f
Jean Meira <jean.meira10@hotmail.com>
2023-01-19 21:01:12 -0300

    feat: add user routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import express from "express";
 
 const globalRoutes = express.Router();
 
-globalRoutes.get("/solutions", (req, res) => res.json({ message: "wow" }));
+globalRoutes.get("/user/create", (req, res) => res.json({ message: "wow" }));
+globalRoutes.get("/user/login", (req, res) => res.json({ message: "wow" }));
 
 export default globalRoutes;
