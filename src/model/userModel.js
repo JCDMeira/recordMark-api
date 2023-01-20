962c3082612ed5a5ac03cdbf57b66979a735aa2c
Jean Meira <jean.meira10@hotmail.com>
2023-01-19 21:08:59 -0300

    feat: encrypt password before to save

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
+import bcrpty from "bcryptjs";
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: trusted, unique: true },
   password: { type: String, required: true },
   created_at: { type: Number, required: true },
   updated_at: { type: Number, required: true },
+});
+
+UserSchema.pre("save", async function encryptPassword() {
+  const passwordHash = await bcrpty.hash(this.password, 10);
+  this.password = passwordHash;
 });
 
 const UserModel = mongoose.model("user", UserSchema);
