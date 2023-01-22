cb2120fca588a2e8144034413a23abe1e634d4fc
Jean Meira <jean.meira10@hotmail.com>
2023-01-22 15:54:20 -0300

    feat: add remark controller

---
+++ src/controllers/remarkController.js
@@ -0,0 +1,13 @@
+import RemarkModel from "../models/RemarkModel";
+
+class RemarkController {
+  static async addRemark({ body }, res) {}
+
+  static async getRemark(req, res) {}
+
+  static async editRemark(req, res) {}
+
+  static async deleteRemark(req, res) {}
+}
+
+export default RemarkController;
