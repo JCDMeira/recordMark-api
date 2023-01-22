918a1e77663dbbbd685045c8aa372c80889dc84b
Jean Meira <jean.meira10@hotmail.com>
2023-01-22 14:27:02 -0300

    feat: add Auth

---
+++ src/middleware/auth.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+
+function Auth(req, res, next) {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization)
+      return res.status(401).json({ message: "missing token" });
+
+    const fragmentedToken = authorization.split(" ");
+
+    if (fragmentedToken.length !== 2)
+      return res.status(403).json({ message: "Poorly structured token" });
+
+    const [bearer, token] = fragmentedToken;
+
+    if (!/^Bearer/i.test(bearer))
+      return res.status(403).json({ message: "Poorly structured token" });
+
+    jwt.verify(token, process.env.TOKEN_ENCRYPT, (error, decod) => {
+      if (error) return res.status(401).json({ message: "Invalid token" });
+      req.userId = decod.indexOf;
+      return next();
+    });
+  } catch ({ message }) {
+    return res.status(404).json({ message });
+  }
+}
+
+export default Auth;
