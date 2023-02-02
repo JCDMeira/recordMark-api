8284e963c2fb63ea737a49cecee843b726556374
Jean Meira <jean.meira10@hotmail.com>
2023-02-02 19:56:09 -0300

    build: prepare to build#6

--- express/api.js
+++ express/api.js
@@ -2,20 +2,20 @@ import express from "express";
 import servless from "serverless-http";
 import cors from "cors";
 import "../src/config/dbConnect.js";
-import globalRoutes from "../src/routes/routes.js";
-import userRoutes from "../src/routes/Auth/userRoutes.js";
-import remarkRouter from "../src/routes/Auth/remarkRoutes.js";
+// import globalRoutes from "../src/routes/routes.js";
+// import userRoutes from "../src/routes/Auth/userRoutes.js";
+// import remarkRouter from "../src/routes/Auth/remarkRoutes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const routes = (app) => {
-  app.use(globalRoutes);
-  app.use(userRoutes);
-  app.use(remarkRouter);
-};
+// const routes = (app) => {
+//   app.use(globalRoutes);
+//   app.use(userRoutes);
+//   app.use(remarkRouter);
+// };
 
 export default app;
 export const handler = servless(app);
