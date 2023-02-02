69e449310bcbffc96371d0e82a5c658bcea8f6d4
Jean Meira <jean.meira10@hotmail.com>
2023-02-01 21:09:09 -0300

    build: prepare to build#2

--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@ import express from "express";
 import "./config/dbConnect.js";
 import routes from "./routes/index.js";
 import dotenv from "dotenv";
+import servless from "serverless-http";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 routes(app);
 app.use("/.netlify/functions/api", routes); // path must route to lambda
 
 export default app;
+export const handler = servless(app);
