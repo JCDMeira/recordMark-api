10af0a179606deb1778562632a30a44d99b85e1e
Jean Meira <jean.meira10@hotmail.com>
2023-04-05 20:38:49 -0300

    build: prepare to build#12

--- express/api.js
+++ express/api.js
@@ -1,7 +1,22 @@
 import express from "express";
 import servless from "serverless-http";
 import cors from "cors";
-import "../src/config/dbConnect.js";
+// import "../src/config/dbConnect.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose.set("strictQuery", true);
+
+mongoose.connect(process.env.CONNECTIONSTRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.log.bind(console, "Connetion error"));
+db.once("open", () => console.log("sucessful connection"));
+
 // import globalRoutes from "../src/routes/routes.js";
 // import userRoutes from "../src/routes/Auth/userRoutes.js";
 // import remarkRouter from "../src/routes/Auth/remarkRoutes.js";
