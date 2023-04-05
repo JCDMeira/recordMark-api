845ddf7300b5e30c14f799d4dc9ca0aab264a293
Jean Meira <jean.meira10@hotmail.com>
2023-04-05 20:32:58 -0300

    build: prepare to build#10

--- express/api.js
+++ express/api.js
@@ -1,10 +1,10 @@
 import express from "express";
 import servless from "serverless-http";
 import cors from "cors";
-import "../src/config/dbConnect.js";
-import globalRoutes from "../src/routes/routes.js";
-import userRoutes from "../src/routes/Auth/userRoutes.js";
-import remarkRouter from "../src/routes/Auth/remarkRoutes.js";
+// import "../src/config/dbConnect.js";
+// import globalRoutes from "../src/routes/routes.js";
+// import userRoutes from "../src/routes/Auth/userRoutes.js";
+// import remarkRouter from "../src/routes/Auth/remarkRoutes.js";
 
 const app = express();
 
